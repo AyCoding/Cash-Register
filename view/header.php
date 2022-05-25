a707576bafe13202c784e6704495643122f515b5
Ertinox-08 <aymericdelange80@gmail.com>
2022-05-25 14:11:47 +0200

    Update

---
+++ view/header.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="left">
+        <div>
+            <a href="#">Produits</a>
+        </div>
+        <div>
+            <a href="#">Acompte</a>
+        </div>
+        <div>
+            <a href="#">Profil</a>
+        </div>
+    </div>
+    <div class="right">
+        <a href="#">
+            <span class="cash">130,00€</span>
+            <span class="user">J. Doe</span>
+        </a>
+    </div>
+</header>
---
+++ view/main.php
@@ -0,0 +1,30 @@
+<main>
+    <table>
+        <thead>
+        <tr>
+            <th>Produits</th>
+            <th>Quantité</th>
+            <th>Combien ?</th>
+            <th>Validation</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+            <td>Coca-Cola</td>
+            <td>15</td>
+            <form action="" method="">
+                <td>
+                    <input type="number" name="number" id="number" placeholder="Number">
+                </td>
+                <td>
+                    <button type="submit">Valide</button>
+                </td>
+            </form>
+        </tr>
+        </tbody>
+
+    </table>
+
+</main>
+
